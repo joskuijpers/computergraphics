6fb9679472b1fb39b02ad214365201b26525943c
Toine Hartman <tjbhartman@gmail.com>
2015-05-14 18:36:42 +0200

    Almost ex 4.

--- ass1/ass1/Exercises.h
+++ ass1/ass1/Exercises.h
@@ -3,6 +3,11 @@
 
 #include <list>
 #include <set>
+#include <vector>
+#include <queue>
+#include <limits>
+
+using namespace std;
 
 ////////////////// Exercise 1 ////////////////////////////////////
 std::pair<float, float> Statistics(const std::list<float>& values)
@@ -72,9 +77,94 @@ public:
 //////////////////////////////////////////////////////////////////
 
 ////////////////// Exercise 4 ////////////////////////////////////
+typedef pair<int, int> line_t;
+
+pair<float, int> max(line_t line, vector<float> levels) {
+    pair<float, int> max = pair<float, int>(numeric_limits<float>::min(), -1);
+
+    for (int i = line.first; i <= line.second; i++) {
+        if (levels[i] > max.first) {
+            max.first = levels[i];
+            max.second = i;
+        }
+    }
+
+    return max;
+}
+
+pair<float, int> max2(line_t line, vector<float> levels) {
+    pair<float, int> max = pair<float, int>(numeric_limits<float>::min(), -1);
+
+    for (int i = line.first + 1; i < line.second; i++) {
+        if (levels[i] > max.first) {
+            max.first = levels[i];
+            max.second = i;
+        }
+    }
+
+    return max;
+}
+
+float volume(line_t line, vector<float> levels) {
+    float volume = 0.f;
+    float edge = min(levels[line.first], levels[line.second]);
+
+    for (int i = line.first + 1; i < line.second; i++)
+        volume += edge - levels[i];
+
+    return volume;
+}
+
+pair<line_t, line_t> split(line_t line, vector<float> levels) {
+    pair<line_t, line_t> l = pair<line_t, line_t>();
+    pair<float, int> m = max(line, levels);
+
+    l.first = line_t(line.first, m.second);
+    l.second = line_t(m.second, line.second);
+
+    cout << "Splitting...(" << m.first << ", " << m.second << ")" << endl;
+
+    return l;
+}
+
 float WaterLevels(std::list<float> heights)
 {
-	return 0;
+    vector<float> levels {heights.begin(), heights.end()};
+    queue<line_t> q = queue<line_t>();
+    float v = 0.f;
+
+    q.push(line_t(0, levels.size() - 1));
+
+    while(!q.empty()) {
+        line_t l = q.front();
+        q.pop();
+
+        pair<float, int> m = max(l, levels);
+
+        cout << "(" << l.first << ", " << l.second << ") ";
+        cout << m.first << ", " << m.second << endl;
+
+        // Max is on the edge
+        if (levels[l.first] == m.first || m.first == levels[l.second]) {
+            cout << "Calculating volume..." << endl;
+
+            // Calculate volume
+            float p =  volume(l, levels);
+            cout << "Volume: " << p << endl;
+
+            v += p;
+        } else {
+            // Split
+            pair<line_t, line_t> s = split(l, levels);
+
+            q.push(s.first);
+            q.push(s.second);
+        }
+    }
+
+    cout << "Total volume: " << v << endl;
+
+    return v;
 }
 //////////////////////////////////////////////////////////////////
 
